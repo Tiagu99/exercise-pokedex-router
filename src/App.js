421cbf9fa3939e5738ea74cb64a431a5860a577d
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-19 11:50:49 -0300

    implementa router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Pokedex from './Pokedex';
 
 function App() {
   return (
-    <div className="App">
-      <h1> My Pokedex </h1>
-      <Pokedex />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1> My Pokedex </h1>
+        <Route exact path="/" component={ Pokedex } />
+      </div>
+    </BrowserRouter>
   );
 }
 
