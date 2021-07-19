09a967544e0c638f15aa4c8572b0277d2725f7d3
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-19 12:08:30 -0300

    implementa link no componente pokemon

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Pokemon extends React.Component {
     render() {
-        const { name, type, averageWeight, image } = this.props.pokemon;
+        const { name, type, averageWeight, image, id } = this.props.pokemon;
 
         return (
-            <div className="pokemon">
-                <div>
-                    <p> {name} </p>
-                    <p> {type} </p>
-                    <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+            <Link to={ `/details/${id}` }>
+                <div className="pokemon">
+                    <div>
+                        <p> {name} </p>
+                        <p> {type} </p>
+                        <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+                    </div>
+                    <img src={image} alt={`${name} sprite`} />
                 </div>
-                <img src={image} alt={`${name} sprite`} />
-            </div>
+            </Link>
         );
     }
 }
